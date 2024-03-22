2f8fc160d110ea45c2decead131b0cae443256fc
Carlos Pineda <cara_p@unicah.edu>
2024-03-21 20:29:33 -0600

    added: agregada navegación en la vista de header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,17 +1,16 @@
 <header class="header">
-    <div class="header__option">
+    <div class="header__title">
         Carlos Pineda
     </div>
-    <div class="header__option">
-        _hello
-    </div>
-    <div class="header__option">
-        _about
-    </div>
-    <div class="header__option">
-        _projects
-    </div>
-    <div class="header__option">
-        _contact_me
-    </div>
+
+    <nav class="header__menu">
+        <ul class="header__menu__list">
+            <li class="header__menu__list__option"><a href="">_hello</a></li>
+            <li class="header__menu__list__option"><a href="">_about</a></li>
+            <li class="header__menu__list__option"><a href="">_projects</a></li>
+            <li class="header__menu__list__option header__menu__list__option__select"><a href="">_blog</a></li>
+            <li class="header__menu__list__option"><a href=""></a></li>
+            <li class="header__menu__list__option"><a href="">_contact_me</a></li>
+        </ul>
+    </nav>
 </header>
