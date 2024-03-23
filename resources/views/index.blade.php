97bf7817d285d9ef851fec9591b3bf93e1e17558
Carlos Pineda <cara_p@unicah.edu>
2024-03-22 20:26:42 -0600

    added: sección de nombre y descripción de stack

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,16 @@
 @extends('layout')
 @section('body')
+    <section class="content">
+        <div class="content__item">
+            <div class="content__item__text">
+                <div class="content__item__text__intro">Hello, I am</div>
+                <div class="content__item__text__name">Carlos Pineda</div>
+                <div class="content__item__text__description-1">Software Developer | Node.js, Java, <br> Spring Boot, PHP, Laravel</div>
+                <div class="content__item__text__description-2">I am Software Developer with a <br>primary focus on back-end development</div>
+            </div>
+        </div>
+        <div class="content__item">
+
+        </div>
+    </section>
 @endsection
