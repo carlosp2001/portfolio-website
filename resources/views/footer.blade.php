636d642ca6ef570370d036964cc508631a935dfa
Carlos Pineda <cara_p@unicah.edu>
2024-03-24 15:18:43 -0600

    added: nuevo logo en la maquetación de hackerrank

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,10 +4,13 @@
             <ul class="footer__content__nav__menu">
                 <li class="footer__content__nav__menu__option footer__content__nav__menu__option-1"><a href="">_find_me</a></li>
                 <li class="footer__content__nav__menu__option footer__content__nav__menu__option-2"><a href=""><img
-                            src="{{asset('svg/linkedin.svg')}}" alt=""></a></li>
+                            src="{{asset('svg/linkedin.svg')}}" alt="" width="100%"></a></li>
                 <li class="footer__content__nav__menu__option footer__content__nav__menu__option-3"><a href=""><img
                             src="{{asset('svg/github.svg')}}" alt=""></a></li>
-                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-4"><a href="">_find_me</a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-4"><a href=""><object type="image/svg+xml" data="{{ asset('svg/hackerrank.svg') }}" width="40rem">
+                            Your browser does not support SVG
+                        </object></a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-5"><a href="">_find_me</a></li>
             </ul>
         </nav>
         <p class="footer__content__copyright">© 2022 Mi Sitio Web. Todos los derechos reservados.</p>
