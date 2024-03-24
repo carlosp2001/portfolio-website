6f2ccc176713342cdbff08e8a845702a4263fb15
Carlos Pineda <cara_p@unicah.edu>
2024-03-23 21:50:49 -0600

    added: layout de footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="footer__content">
+        <nav class="footer__content__nav">
+            <ul class="footer__content__nav__menu">
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-1"><a href="">_find_me</a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-2"><a href=""><img
+                            src="{{asset('svg/linkedin.svg')}}" alt=""></a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-3"><a href=""><img
+                            src="{{asset('svg/github.svg')}}" alt=""></a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-4"><a href="">_find_me</a></li>
+            </ul>
+        </nav>
+        <p class="footer__content__copyright">© 2022 Mi Sitio Web. Todos los derechos reservados.</p>
+    </div>
+
+</footer>
