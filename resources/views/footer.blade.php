b416183bd9f823d0d5e3ae2918540e36a605a0da
Carlos Pineda <cara_p@unicah.edu>
2024-03-25 21:36:46 -0600

    fixed: estructura uso de img a cambio de svg

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,21 +2,27 @@
     <div class="footer__content">
         <nav class="footer__content__nav">
             <ul class="footer__content__nav__menu">
-                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-1"><a href="">_find_me</a></li>
-                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-2"><a href=""><img
-                            src="{{asset('svg/linkedin.svg')}}" alt="" width="100%"></a></li>
-                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-3"><a href=""><img
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-1"><a
+                        href="">_find_me</a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-2"><a
+                        href="https://www.linkedin.com/in/carlos-pineda-a1a780220/"><img
+                            src="{{asset('svg/linkedin.svg')}}" alt="linkedin-logo" width="100%"></a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-3"><a
+                        href="https://github.com/carlosp2001/"><img
                             src="{{asset('svg/github.svg')}}" alt=""></a></li>
-                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-4"><a href=""><object type="image/svg+xml" data="{{ asset('svg/hackerrank.svg') }}" width="40rem">
-                            Your browser does not support SVG
-                        </object></a></li>
-                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-4"><a href=""><object type="image/svg+xml" data="{{ asset('svg/medium.svg') }}" width="40rem">
-                            Your browser does not support SVG
-                        </object></a></li>
-                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-5"><a href="">_find_me</a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-4"><a
+                        href="https://www.hackerrank.com/profile/cara_p/">
+                        <img src="{{asset('svg/hackerrank.svg')}}" alt="hackerrank-logo" width="40rem">
+                    </a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-4"><a
+                        href="https://medium.com/@pinedalberto">
+                        <img src="{{asset('svg/medium.svg')}}" alt="medium-logo" width="40rem">
+                    </a></li>
+                <li class="footer__content__nav__menu__option footer__content__nav__menu__option-5"><a
+                        href="">_find_me</a></li>
             </ul>
         </nav>
-        <p class="footer__content__copyright">© 2022 Mi Sitio Web. Todos los derechos reservados.</p>
+        <p class="footer__content__copyright">© 2024 Carlos Pineda. Todos los derechos reservados.</p>
     </div>
 
 </footer>
