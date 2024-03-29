610ded34ca3abb5be62f4d88b4d2b0f1e392b7fd
Carlos Pineda <cara_p@unicah.edu>
2024-03-29 10:13:49 -0600

    updated: aumentado el tamaño del texto de descripción

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,10 +5,10 @@
 
     <nav class="header__menu">
         <ul class="header__menu__list">
-            <li class="header__menu__list__option"><a href="">_hello</a></li>
+            <li class="header__menu__list__option header__menu__list__option__select"><a href="">_hello</a></li>
             <li class="header__menu__list__option"><a href="">_about</a></li>
             <li class="header__menu__list__option"><a href="">_projects</a></li>
-            <li class="header__menu__list__option header__menu__list__option__select"><a href="">_blog</a></li>
+            <li class="header__menu__list__option"><a href="">_blog</a></li>
             <li class="header__menu__list__option"><a href=""></a></li>
             <li class="header__menu__list__option"><a href="">_contact_me</a></li>
         </ul>
