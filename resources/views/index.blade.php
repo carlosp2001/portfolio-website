007358031051ea72c979479a70561ce9a5206278
Carlos Pineda <cara_p@unicah.edu>
2024-03-30 21:55:00 -0600

    updated: cambio del display de la información del proyecto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,19 +17,23 @@
             <div class="content__item__projects">
                 <div class="content__item__projects__navbar__container">
                     <div class="content__item__projects__navbar">
-                        <button type="button"
-                                class="content__item__projects__navbar__button content__item__projects__navbar__button__active"></button>
-                        <button type="button" class="content__item__projects__navbar__button"></button>
-                        <button type="button" class="content__item__projects__navbar__button"></button>
+                        @foreach($projects as $project)
+                            <button type="button"
+                                    class="content__item__projects__navbar__button {{$loop->first ? 'content__item__projects__navbar__button__active' : ''}}"></button>
+                        @endforeach
                     </div>
                 </div>
-                <div class="content__item__projects__project">
-                    <div class="content__item__projects__project__container"><span
-                            class="content__item__projects__project__title">_gofood</span> <span
-                            class="content__item__projects__project__date">Sep 2023 - Current Working</span></div>
-                    <div style="background-image: url('{{asset("images/gofoodproject.png")}}')"
-                         class="content__item__projects__project__image">
-                    </div>
+                <div class="content__item__projects__container">
+                    @foreach($projects as $project)
+                        <div class="content__item__projects__project {{$loop->first ? 'content__item__projects__project__active' : ''}}">
+                            <div class="content__item__projects__project__container"><span
+                                    class="content__item__projects__project__title">_gofood</span> <span
+                                    class="content__item__projects__project__date">Sep 2023 - Current Working</span></div>
+                            <div style="background-image: url('{{asset("images/gofoodproject.png")}}')"
+                                 class="content__item__projects__project__image">
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $json = file_get_contents(storage_path('app_data/projects.json'));
+
+    $json_data = json_decode($json, true);
+
     $activePage = 'home';
-    return view('index', compact('activePage'));
+    return view('index', ['projects' => $json_data, 'activePage' => $activePage]);
 });
